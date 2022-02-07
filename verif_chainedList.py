830fca8bb076b16d53ecbf1f1f69b50543b245b8
Aimen CHERIF <cherif_aymenn@hotmail.com>
2022-02-07 21:52:07 +0100

    added unittest

---
+++ verif_chainedList.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Aimen CHERIF'
+
+
+import unittest
+from chainedList import ChainedList
+
+
+class TestChainedList(unittest.TestCase):
+    """ This class test functions of ChainedList class """
+
+    def setUp(self):
+        """ goes for initialization """
+        self.test_list = ChainedList()
+        self.another_test_list = ChainedList()
+
+    def test_chainedList_is_empty(self):
+        """ tests if a new linked list is empty """
+        self.assertIsNone(self.test_list.first_node)
+
+    def test_new_node(self):
+        """tests if a linked list on which we've just stacked an element is
+        not empty"""
+
+        self.test_list.insert_node_beginning(8)
+        self.assertIsNotNone(self.test_list.first_node)
+
+    def test_add_delete_node_equals(self):
+        """ tests if a linked list stacked then unstacked with elements is unchanged"""
+        self.another_test_list.insert_node_beginning(7)
+        self.another_test_list.delete_node(7)      
+        self.assertEqual(self.another_test_list, self.test_list)
+        #to compare the two objects i've added __eq__method in ChainedList class
+
+
+    def test_added_top_equals_top(self):
+        """ Tests if the top of a linked list on which we have just stacked an element
+        e is e """
+        self.test_list.insert_node_beginning(9)
+        self.assertEqual(self.test_list.first_node.data, 9)
+
+
+if __name__ == '__main__':
+    unittest.main()
